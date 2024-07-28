d10b77b12142394d7fc74aaa6bca754fc05b3ee4
Alexandre Devert <marmakoide@hotmail.fr>
2024-07-28 11:56:20 +0200

    Removed printf statement in parser

--- src/node.c
+++ src/node.c
@@ -109,7 +109,8 @@ Node_create_by_name(
 ) {
 	assert(name != 0);
 	assert(name->data != 0);
-	assert(name->delegate_data != 0);
+	assert(delegate_name != 0);
+	assert(delegate_name->data != 0);
 
 	const NodeDelegate** delegate_ptr = node_delegate_list;
 	for( ; *delegate_ptr != 0; ++delegate_ptr)
