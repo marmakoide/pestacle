16e90009ca8067bc274f9fc411cdb64b9e58b710
Alexandre Devert <alexandre.devert@algokhod.com>
2024-09-23 09:34:44 +0200

    Updated config.mk

--- config.mk
+++ config.mk
@@ -1,22 +1,19 @@
 # build mode, dbg=debug rls=release
 MODE=rls
 
-# paths
-INCLUDES=-I./include
-LIBS=\
-$(shell pkg-config --libs libavcodec) \
-$(shell pkg-config --libs libavformat) \
-$(shell pkg-config --libs libavutil) \
-$(shell pkg-config --libs libpng) \
-$(shell sdl2-config --libs) \
-$(shell pkg-config --libs zlib) \
--lm
+# libraries
+LIBS += $(shell pkg-config --libs libavcodec)
+LIBS += $(shell pkg-config --libs libavformat)
+LIBS += $(shell pkg-config --libs libavutil)
+LIBS += $(shell pkg-config --libs libpng)
+LIBS += $(shell sdl2-config --libs)
+LIBS += $(shell pkg-config --libs zlib)
+LIBS += -lm
 
 # flags setup
-CFLAGS += -std=c11 -Wall -Wextra -Wno-deprecated-declarations
+CFLAGS += -std=c11 -Wall -Wextra -Werror -Wno-deprecated-declarations
 
 ifeq ($(MODE), rls)
-CFLAGS += -Werror
 CFLAGS += -flto
 CFLAGS += -DNDEBUG -Os -fomit-frame-pointer -mtune=generic -fno-align-functions -fno-align-jumps -fno-align-labels -fno-align-loops -fno-exceptions -fno-unwind-tables -fno-asynchronous-unwind-tables 
 endif
@@ -24,6 +21,9 @@ endif
 ifeq ($(MODE), dbg)
 CFLAGS += -DDEBUG -O0 -g -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer
 endif
+
+# include paths
+INCLUDES=-I./include
 
 CFLAGS += $(INCLUDES)
 CFLAGS += $(shell pkg-config --cflags libavcodec)
