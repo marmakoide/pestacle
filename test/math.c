bcac1c869d3586aef43b4093577d5c22a8a87feb
Alexandre Devert <alexandre.devert@algokhod.com>
2024-09-27 11:50:59 +0200

    Added unit test for Vector_add

--- test/math.c
+++ test/math.c
@@ -48,6 +48,30 @@ MU_TEST(test_Vector_copy) {
 }
 
 
+MU_TEST(test_Vector_add) {
+	Vector U, V;
+
+	for(size_t i = 1; i < 256; ++i) {
+		Vector_init(&U, i);
+		Vector_arange(&U, (real_t)0, (real_t)1);
+		
+		Vector_init(&V, i);
+		Vector_copy(&V, &U);
+
+		Vector_add(&U, &V);
+
+		for(size_t j = 0; j < i; ++j)
+			mu_assert_double_eq(
+				Vector_get_coeff(&U, j),
+				2 * Vector_get_coeff(&V, j)
+			);
+
+		Vector_destroy(&U);
+		Vector_destroy(&V);
+	}
+}
+
+
 MU_TEST(test_Vector_scale) {
 	Vector U;
 
@@ -167,6 +191,7 @@ MU_TEST(test_Vector_convolution) {
 MU_TEST_SUITE(test_suite) {
 	MU_RUN_TEST(test_Vector_fill);
 	MU_RUN_TEST(test_Vector_copy);
+	MU_RUN_TEST(test_Vector_add);
 	MU_RUN_TEST(test_Vector_scale);
 	MU_RUN_TEST(test_Vector_sum);
 	MU_RUN_TEST(test_Vector_square_sum);
